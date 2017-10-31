5a79f2bcc25895d5102fde6baab2718cc8789317
etilly14 <32934368+etilly14@users.noreply.github.com>
2017-10-31 10:42:54 -0500

    Update phi.m

--- phi.m
+++ phi.m
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 function y=phi(x,L)
 mask = (x>-L)&(x<L);
 y = mask.*(L-x.^2);
 end
-=======
-function y=phi(x, L)
-mask = (x>-L)&(x<L);
-y = mask.*(L-x.^2);
-end
->>>>>>> upstream/master
+
