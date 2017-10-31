6d214a920f318f8fc1141d708882ae5a39c89e39
scoldfield96 <32934372+scoldfield96@users.noreply.github.com>
2017-10-31 10:27:03 -0500

    Update phi.m

--- phi.m
+++ phi.m
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 function y=phi(x, L)
 mask = (x>-L)&(x<L);
 y = mask.*(L-x.^2);
-=======
-function y=phi(x)
-mask = (x>-1)&(x<1);
-y = mask.*(1-x.^2);
->>>>>>> e7b251407bf5dc093c634e5373bd947b81e85d58
 end
